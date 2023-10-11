6fac97b49f9d0eb0b18ebf47ac226965736d917f
Dennis Chow <dennischow@Denniss-MBP>
2023-10-11 23:17:08 +0800

    FCP-NEXT Moved meta tag to _app to resolve warning

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Head from "next/head";
 
 import "./../assets/styles/index.scss";
 
@@ -42,5 +43,17 @@ export default function App({ Component, pageProps }) {
         fetchData();
     }, []);
 
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="format-detection" content="telephone=no" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
+                <link rel="apple-touch-icon" sizes="512x512" href="/logo512.png" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
