99e0aa496321725ad30bd435cd8e7d8734edf6d9
Dennis Chow <dennischow@Denniss-MBP>
2023-10-14 11:53:33 +0800

    FCP-NEXT Updated config to railingSlash-true

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
         includePaths: [path.join(__dirname, "assets/styles")],
         // prependData: `@import "./src/assets/styles/index.scss";`,
     },
+    trailingSlash: true,
 };
 
 module.exports = nextConfig;
